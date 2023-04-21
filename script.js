80ab80d02f9fb04882030a7f94239a79a46adf6e
GabriellaSwenson <unipuppydj@gmail.com>
2023-04-20 20:26:04 -0500

    Updated Javascript and HTML

--- script.js
+++ script.js
@@ -8,16 +8,46 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  $(".saveBtn").on("click", function () {
+    var id = $(this).parent().attr("id");
+    var description = $(this).siblings(".description").val();
+
+    // save the user input in local storage using the time-block id as the key
+    localStorage.setItem(id, description);
+  });
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  var currentHour = dayjs().hour(); // get the current hour in 24-hour time
+
+  $(".time-block").each(function () {
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  $(".time-block").each(function () {
+    var id = $(this).attr("id");
+    var description = localStorage.getItem(id);
+
+    if (description !== null) {
+      $(this).find(".description").val(description);
+    }
+  });
   //
   // TODO: Add code to display the current date in the header of the page.
+  var currentDate = dayjs().format("dddd, MMMM D");
+  $("#currentDay").text(currentDate);
 });
